da4d8503d3971e16d97a17e51485ebf319526fd0
gogurtenjoyer <36354352+gogurtenjoyer@users.noreply.github.com>
2024-01-05 22:55:18 -0500

    transformers/quotation mark fixes
    Fixes:
    - reduces transformers complaining about pad/end tokens
    - properly escapes `"` marks so that they are passed to Compel intact

--- nightmare.py
+++ nightmare.py
@@ -3,11 +3,11 @@ from typing import Literal, Union
 import random as r
 import re
 
-#import torch
-
 from omegaconf import OmegaConf
 from pydantic import BaseModel, Field
 from transformers import pipeline
+
+# from invokeai.backend.util.devices import choose_torch_device, choose_precision
 
 from invokeai.app.invocations.baseinvocation import (
     BaseInvocation,
@@ -31,7 +31,13 @@ except:
     conf = OmegaConf.load(DEFAULT_CONF_PATH)
 MODEL_REPOS = Literal[tuple(conf['Nightmare']['Models'])]
 REPLACE = conf['Nightmare']['Replace']
+# MEM_CACHE = True
 
+# dev = choose_torch_device()
+# precis = choose_precision(dev)
+# print("***NIGHTMARE PROMPTGEN DEBUG***")
+# print(f"device: {dev}, precision: {precis}")
+# print("*"*30)
 
 @invocation_output("nightmare_str_output")
 class NightmareOutput(BaseInvocationOutput):
@@ -40,7 +46,7 @@ class NightmareOutput(BaseInvocationOutput):
 
 
 @invocation("nightmare_promptgen", title="Nightmare Promptgen", tags=["nightmare", "prompt"],
-            category="prompt", version="1.0.1", use_cache=False)
+            category="prompt", version="1.0.2", use_cache=False)
 class NightmareInvocation(BaseInvocation):
     """makes new friends"""
 
@@ -54,11 +60,10 @@ class NightmareInvocation(BaseInvocation):
 
     def loadGenerator(self, repo_id: str):
         """loads the tokenizer, model, etc for the generator"""
-        
-        # if you'd like to try hardware acceleration, add the 'device' arg below, like
-        # 'device=0' or'torch.device("cuda")' etc - import torch above if the latter  - example:
-        
-        # generator = pipeline(model=repo_id, tokenizer=repo_id, task="text-generation", device=torch.device("mps"))
+
+        # TODO: determine if hardware accel is even worth it at all       
+        # generator = pipeline(model=repo_id, tokenizer=repo_id, task="text-generation", device=dev)
+
         generator = pipeline(model=repo_id, tokenizer=repo_id, task="text-generation")
         return generator
 
@@ -74,19 +79,37 @@ class NightmareInvocation(BaseInvocation):
         return phrase
 
 
+    # def splitPrompt(text):
+    #     """ If the prompt is too long, let's split it up for blend/and """
+    #     puncts = ['.', ',', ';', '--']
+    #     splitted = []
+    #     while len(text) > 300:
+    #         cut_where, cut_why = max((text.rfind(punc, 0, 293), punc) for punc in puncts)
+    #         if cut_where <= 0:
+    #             cut_where = text.rfind(' ', 0, 293)
+    #             cut_why = ' '
+    #         cut_where += len(cut_why)
+    #         splitted.append(text[:cut_where].rstrip())
+    #         text = text[cut_where:].lstrip()
+    #     splitted.append(text)
+    #     return splitted
+
+
     def makePrompts(self, prompt: str, temp: float, p: float, k: int):
         """loads textgen model, generates a (str) prompt, unloads model"""
         generator = self.loadGenerator(self.repo_id)
         output = generator(prompt, max_new_tokens=300, temperature=temp,
                             do_sample=True, top_p=p, top_k=k,
-                            num_return_sequences=1)
+                            num_return_sequences=1,
+                            pad_token_id=generator.tokenizer.eos_token_id)
         del generator
-        return self.censor(output[0]['generated_text'])
+        return self.censor(output[0]['generated_text'].strip())
 
 
     def invoke(self, context: InvocationContext) -> NightmareOutput:
         """ does the thing """
-        generated = self.makePrompts(self.prompt, self.temp, self.top_p, self.top_k)
+        unescaped = self.makePrompts(self.prompt.strip(), self.temp, self.top_p, self.top_k)
+        generated = unescaped.replace('"', r'\"').strip()
         nl, bl, nr = "\n", "\033[1m", "\033[0m"
-        context.services.logger.info(f"{nl}{nl}*** YOUR {bl}NIGHTMARE{nr} IS BELOW ***{nl}{generated}{nl}")
+        context.services.logger.info(f"{nl}{nl}*** YOUR {bl}NIGHTMARE{nr} IS BELOW ***{nl}{unescaped}{nl}")
         return NightmareOutput(prompt=generated)
